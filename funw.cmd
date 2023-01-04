e1a6fd749202bd55803cd35bda62796983e7d532
xwiz <c.opata@robosyslive.com>
2023-01-05 00:01:22 +0100

    Generate scaffold and api separately

--- funw.cmd
+++ funw.cmd
@@ -1,1 +1,2 @@
-php artisan infyom:api_scaffold "%1" --fieldsFile="%2" --relations --no-interaction --skip="dump-autoload,dumpautoload,dump_autoload,menu"
+php artisan infyom:scaffold "%1" --fieldsFile="%2" --relations --no-interaction --skip="dump-autoload,dumpautoload,dump_autoload,menu"
+php artisan infyom:api "%1" --fieldsFile="%2" --relations --no-interaction --skip="dump-autoload,dumpautoload,dump_autoload,menu"
